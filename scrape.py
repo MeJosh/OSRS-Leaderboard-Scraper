47adf8026f7b16f0403038e7d908fdea72f024a9
Josh <lekgold@gmail.com>
2023-05-07 16:36:31 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -9,23 +9,25 @@ base_url = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?playe
 # Name of the directory containing the player data files
 data_dir = "data"
 
-# Get a list of all directories in the data directory
-dirs = [name for name in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, name))]
+# Get a list of all files (not directories) in the data directory
+files = [name for name in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, name))]
 
 # Get the current date in YYYY_MM_DD format
 current_date = datetime.date.today().strftime('%Y_%m_%d')
 
-# Loop through each directory and scrape the player data
-for dirname in dirs:
+# Loop through each file and scrape the player data
+for filename in files:
     # Load the player data from the file
-    with open(os.path.join(data_dir, dirname), 'r') as f:
+    with open(os.path.join(data_dir, filename), 'r') as f:
         player_data = f.read().strip()
 
     # Encode the player data and append it to the base URL
     url = base_url + urllib.parse.quote(player_data)
 
     # Make a request to the URL and save the response to a file
-    filename = os.path.join(data_dir, f"{dirname}_{current_date}.txt")
+    dirname = os.path.splitext(filename)[0]
+    output_filename = f"{dirname}_{current_date}.txt"
+    output_path = os.path.join(data_dir, output_filename)
     response = requests.get(url)
-    with open(filename, 'wb') as f:
+    with open(output_path, 'wb') as f:
         f.write(response.content)
